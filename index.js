2bb8c24676b332ce486c8cd5ef17de0e220914e9
Ankit Shaji <ankitshaji@gmail.com>
2022-09-14 15:15:30 +1200

    initialize express app

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express"); //functionObject //express module
+const path = require("path"); //pathObject //path module
+const app = express(); //appObject
+// ********************************************************************************
+//main express application/appObject - (RESTful) webApi - webApi using REST principles
+// *******************************************************************************
+//mongoose ODM - supports promises
+const mongoose = require("mongoose"); //mongooseObject //mongoose module
